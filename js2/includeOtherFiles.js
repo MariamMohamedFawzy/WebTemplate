8c2d682bad652ccb4c1b9fdcd0376e26af9124b3
Apple <apple@Apples-MacBook-Pro-5.local>
2016-02-23 18:23:19 +0200

    first commit

---
+++ js2/includeOtherFiles.js
@@ -0,0 +1,11 @@
+/**
+ * Created by apple on 2/22/16.
+ */
+
+$(function(){
+    $("#main-header").load("header.html");
+    $("#sidebar").load("sidebar.html");
+    $("#footer").load("footer.html");
+
+    $("#guest-header").load("guestHeader.html");
+});
